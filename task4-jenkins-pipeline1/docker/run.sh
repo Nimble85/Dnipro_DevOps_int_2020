a9c9d3ef361ba6e6ca5e931ca931399403c360a7
fenixra73 <fenixra73@gmail.com>
2020-02-08 01:45:38 +0200

    add new files

---
+++ task4-jenkins-pipeline1/docker/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp  ${REF}/plugins/*.jpi ${JENKINS_HOME}/plugins \
+   && echo "List of plugins in folder ${JENKINS_HOME}/plugins" && ls -la ${JENKINS_HOME}/plugins
+
+java -jar -Djenkins.install.runSetupWizard=false /usr/share/jenkins/jenkins.war 
