f6fabd1ad1d7dd31f9b2292922fc5c412f33db17
fenixra73 <fenixra73@gmail.com>
2020-02-08 19:23:55 +0200

    fix run.sh

--- task4-jenkins-pipeline1/docker/run.sh
+++ task4-jenkins-pipeline1/docker/run.sh
@@ -3,4 +3,5 @@
 cp  ${REF}/plugins/*.jpi ${JENKINS_HOME}/plugins \
    && echo "List of plugins in folder ${JENKINS_HOME}/plugins" && ls -la ${JENKINS_HOME}/plugins
 
+/usr/sbin/nginx
 java -jar -Djenkins.install.runSetupWizard=false /usr/share/jenkins/jenkins.war 
