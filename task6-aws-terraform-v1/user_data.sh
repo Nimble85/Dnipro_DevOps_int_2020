46759e8f90ccbd9717499484d2bc21fc2febdda9
fenixra73 <fenixra73@gmail.com>
2020-03-10 00:55:47 +0200

    fix boot script ec2

--- task6-aws-terraform-v1/user_data.sh
+++ task6-aws-terraform-v1/user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum update -y
 sudo amazon-linux-extras install -y nginx1
-sudo aws sync s3://fenixra-site /usr/share/nginx/html/
+sudo aws s3 sync s3://fenixra-site /usr/share/nginx/html/
 sudo chown nginx:nginx -R /usr/share/nginx/html/
 sudo systemctl enable nginx
 sudo systemctl start nginx
